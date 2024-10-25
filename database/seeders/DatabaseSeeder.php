4c2daf67d7acc4d6adc8b8f103a1b2eb97057e0e
NthnamUtd <nthnam.dev@gmail.com>
2024-10-25 14:35:17 +0700

    $

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,16 +7,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+    
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        $this->call(UserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'name' => 'Supper Admin',
+        //     'email' => 'nthnam.dev@gmail.com',
         // ]);
     }
 }
